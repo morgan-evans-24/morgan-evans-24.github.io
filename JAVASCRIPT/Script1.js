59daf61bd806d5a868b1328d5901286f942cd44a
PutMercury <115659480+PutMercury@users.noreply.github.com>
2024-09-18 09:13:03 +0100

    reworked query submission

--- JAVASCRIPT/Script1.js
+++ JAVASCRIPT/Script1.js
@@ -59,14 +59,12 @@ bars.forEach(bar => {
     barObserver.observe(bar)
 });
 
-console.log("Why");
 
 const headers = document.querySelectorAll("h2");
 
 const headerObserver = new IntersectionObserver(entries => {
     entries.forEach(entry => {
         if (entry.isIntersecting) {
-            console.log("should be working")
             highlightNavElement(entry.target)
         }
     })
@@ -85,6 +83,44 @@ changeClass(0);
 
 window.onscroll = function () { scrollFunction() };
 
+const form = document.getElementById('form');
+const result = document.getElementById('result');
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const formData = new FormData(form);
+    const object = Object.fromEntries(formData);
+    const json = JSON.stringify(object);
+    result.innerHTML = "Please wait..."
+
+    fetch('https://api.web3forms.com/submit', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: json
+    })
+        .then(async (response) => {
+            let json = await response.json();
+            if (response.status == 200) {
+                result.innerHTML = json.message;
+            } else {
+                console.log(response);
+                result.innerHTML = json.message;
+            }
+        })
+        .catch(error => {
+            console.log(error);
+            result.innerHTML = "Something went wrong!";
+        })
+        .then(function () {
+            form.reset();
+            setTimeout(() => {
+                result.style.display = "none";
+            }, 3000);
+        });
+});
 
 function loadBar(bar) {
     bar.style.width = "0%";
